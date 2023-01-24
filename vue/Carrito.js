85a0d3ef0171ba1019c2584d502fb7595c6d93c6
unknown <ffrfdke86@gmail.com>
2023-01-24 09:49:34 -0600

    24/01/23

---
+++ vue/Carrito.js
@@ -0,0 +1,18 @@
+Vue.createApp({
+    data(){
+        return{
+            carrito:[
+              
+            ],
+            producto:""
+        }
+    },
+    methods:{
+        agregarProducto(){
+            this.carrito.push(this.producto);
+            this.producto="";
+        }
+    }
+
+    },
+).mount("#CR")
