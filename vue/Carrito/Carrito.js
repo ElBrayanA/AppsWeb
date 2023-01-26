93de78f8280d729f26eeb7131b58f93fdbdfd3ea
unknown <ffrfdke86@gmail.com>
2023-01-26 14:55:45 -0600

    26/01/2023

--- vue/Carrito/Carrito.js
+++ vue/Carrito/Carrito.js
@@ -1,18 +1,39 @@
 Vue.createApp({
-    data(){
-        return{
-          /*   carrito:[
-              
-            ],
-            producto:"" */
+    data() {
+        return {
+            /*   carrito:[
+                
+              ],
+              producto:"" */
+              nombre:"",
+              precio:"",
+              cantidad:"",
+              color:"",
+              departamento:"",
+              fecha:"",
+            productos: []
         }
     },
-    methods:{
-       /*  agregarProducto(){
-            this.carrito.push(this.producto);
-            this.producto="";
-        } */
+    methods: {
+        /*  agregarProducto(){
+             this.carrito.push(this.producto);
+             this.producto="";
+         } */
+        AgregarProductos(){
+           const producto ={
+            nombre: this.nombre,
+            precio: this.precio,
+            cantidad: this.cantidad,
+            color: this.color,
+            departamento:this.departamento,
+            fecha:this.fecha
+           }
+           this.productos.push(producto)
+        },
+        clear(){
+            this.productos=""
+        }
     }
 
-    },
+},
 ).mount("#CR")
