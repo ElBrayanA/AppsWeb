b645134a644057ee2fd5446de41a5250e335ec3f
unknown <ffrfdke86@gmail.com>
2023-02-16 14:51:33 -0600

    16/02/2023

--- P2/API POKEMON/vue.js
+++ P2/API POKEMON/vue.js
@@ -1,7 +1,8 @@
 Vue.createApp({
     data() {
         return {
-            Pokemon:null
+            pokemones:"",
+            Pokemon:[]
         }
     }, methods: {
         /*  tipo de peticiones
@@ -10,12 +11,18 @@ Vue.createApp({
          Put ---- modificar recursos
          Delete --- elimminar recursos */
         async getPokemon() {
-            const random = Math.floor(Math.random() * 630);
-            const response = await fetch('https://pokeapi.co/api/v2/pokemon/'+ random, {
+            for(i=1;i<=50;i++){
+/*             const random = Math.floor(Math.random() * 630);
+ */            const response = await fetch('https://pokeapi.co/api/v2/pokemon/'+ i, {
                 method: 'GET'
             });
             const data = await response.json();
             this.Pokemon=data;
+            this.pokemons={
+                pokemones:response
+            }
+            this.pokemons.push()
+        }
         }
     }
 }
